561b7cc6edea53b2830ec75b4c66ac6af19c91de
FA <finneganadair@gmail.com>
2026-02-05 22:27:54 -0500

    I messed up big time

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ celso = []
 with open(filename) as f:
   skibididata = f.read().split() ##hell yeah numbers and crap
   for i in skibididata:
+    count = 0
     dubble.append(float(i))
-    celso.append((int(100 * ((5 * (dubble[i] - 32)) / 9.0))) / 100) ## cough cough why
+    celso.append((int(100 * ((5 * (dubble[count] - 32)) / 9.0))) / 100) ## cough cough why
+    count += 1
 
 os.remove("main.py") ## happy now?
   
