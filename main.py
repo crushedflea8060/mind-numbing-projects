af9f40cfe9131a2addac62f57e4f5d8ebd47f824
FA <finneganadair@gmail.com>
2026-02-06 09:50:34 -0500

    Fix count variable handling in main.py
    Refactor count initialization and usage in main.py.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ dubble = []
 celso = []
 with open(filename) as f:
   skibididata = f.read().split() ##hell yeah numbers and crap
+  count = 0
   for i in skibididata:
-    count = 0
     dubble.append(float(i))
     celso.append((int(100 * ((5 * (dubble[count] - 32)) / 9.0))) / 100) ## cough cough why
     count += 1
